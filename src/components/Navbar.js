031b1facd379d7f824babab7fd0070ff91782841
jchiappetta <jchiappetta@devexperts.com>
2021-11-04 17:15:51 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
     <div className="ui three item menu">
-        <NavLink to="/tweets" className="item" target="_self">
+        <NavLink to="/tweets" className="item" target="_blank">
             New Tweets
         </NavLink>
         <NavLink to="/rules" className="item" target="_blank">
             Manage Rules
         </NavLink>
-        <NavLink to="/status" className="item" target="_self">
+        <NavLink to="/status" className="item" target="_blank">
             Status Pages
         </NavLink>
     </div>
