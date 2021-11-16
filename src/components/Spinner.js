eee69fd4fb78567eb69184766f469537586992cf
jchiappetta <jchiappetta@devexperts.com>
2021-11-16 11:16:33 -0500

    removed images folder from public

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import twitterLogo from '../images/Twitter_Logo_Blue.png';
 
 const Spinner = () => (
     <div style={{ padding: 12 }}>
         <div className="ui active centered large inline loader">
             <img
                 className="ui image"
-                src="/Twitter_Logo_Blue.png"
-                alt="Twitter Logo"
+                src={twitterLogo}
+                alt="logo"
             />
         </div>
     </div>
